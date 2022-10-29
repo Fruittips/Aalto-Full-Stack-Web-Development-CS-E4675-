c31d76b31c0b881c3dfa0ac1752bd95a461b7077
Fruittips <yida_mah@mymail.sutd.edu.sg>
2022-10-29 23:23:26 +0300

    completed tilll 4.18, Token Auth

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -8,6 +8,7 @@ const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 logger.info("connecting to ", config.MONGODB_URI);
 
@@ -19,6 +20,7 @@ app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 
